70a8d5f55b4ad5780be4b80e259a55c0d940d925
bss-blip <bati.doh@yahoo.com>
2024-08-13 02:02:35 +0900

    기본 명력어 처리

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -1,9 +1,18 @@
 package com.sbs.java.board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("== 게시판 작동 ==");
-    System.out.println(" 게시판을 시작합니다. ");
-    System.out.println("== 게시판 종료 ==");
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("== 자바 텍스트 게시판 ==");
+    System.out.println("명령) ");
+    String cmd = sc.nextLine();
+
+
+    System.out.printf("입력받은 명력어  : %s\n", cmd);
+    System.out.println("== 자바 텍스트 게시판 종료 ==");
+
+    }
   }
-}
