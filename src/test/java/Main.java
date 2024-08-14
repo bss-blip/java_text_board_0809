5f6bf1bd89577e67f15efdb1e93d4f912bb6697f
bss-blip <bati.doh@yahoo.com>
2024-08-14 20:17:21 +0900

    split을 이용하여 문장 쪼개기

---
+++ src/test/java/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+    public class Main {
+      public static void main(String[] args) {
+
+        String queryString = "a=1&b=2&c=3";
+        String[] queryStringBits = queryString.split("&");
+
+        System.out.println(Arrays.toString(queryStringBits));
+
+
+        for(String bit : queryStringBits) {
+          System.out.println(bit);
+
+
+    }
+  }
+}
